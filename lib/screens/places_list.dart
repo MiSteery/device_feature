6716322f20689b874f51d003a257e19961485ef9
shweta <shweta.kc016@gmail.com>
2021-07-06 13:48:07 +0545

    feat: A first screen

---
+++ lib/screens/places_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PlacesList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Places'),
+        
+      ),
+      body: Center(
+        child: Text('Hey'),
+      ),
+      
+    );
+  }
+}
